a76178f3d6092c7bdbb3bd292a8ea31a792b25e0
Emeric FOKOUONG TOUOLAC <bestvemeric@gmail.com>
2023-09-26 03:08:09 -0500

    ADDED SOME FEATURE TO PORTFOLIO

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
-# Portfolio
+# Emeric's Portfolio
 
-Perfect Portoflio Template to start.
-Watch my YouTube video to see how to run it.
-Video how to use it:
-https://youtu.be/ddLuZXs8Uyw
-Video how to deploy it:
-https://youtu.be/E1dqffpUlMA
+Welcome to my professional portfolio, where I showcase my skills, projects, and experiences. This portfolio is designed to give you a comprehensive look into my journey as a [Your Profession/Role, e.g., "Web Developer"].
 
-## Credits
+For any inquiries, reach out to me on [bestvemeric@example.com].
 
-Template was created by Tenzin Phuljung and edited by Jakob Wolitzki.
+## Instructions
+
+1. **How to Run the Portfolio Locally**
+   - Instructions specific to your portfolio setup.
+   - e.g., "Clone the repository, navigate to the root directory, and open `index.html` in your favorite browser."
+
+2. **Deployment Guidelines**
+   - Steps to deploy your portfolio to a server or platform.
+   - e.g., "Deploy using Netlify: Simply connect your repository, and Netlify will handle the rest."
+
+## Acknowledgements
+
+The original template was crafted by Tenzin Phuljung. Modifications and customizations were made by Emeric with inspiration from Jakob Wolitzki.
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,84 +1,122 @@
-const Portfolio = function() {
-	function makeWords() {
-		var words = [
-			{
-				text: "rapping",
-				weight: 12.3
-			}, {
-				text: "css3",
-				weight: 8
-			}, {
-				text: "javascript",
-				weight: 14
-			}, {
-				text: "jquery",
-				weight: 3
-			}, {
-				text: "programming",
-				weight: 7
-			}, {
-				text: "python",
-				weight: 10
-			}, {
-				text: "java",
-				weight: 9
-			}, {
-				text: "qazi",
-				weight: 15
-			}, {
-				text: "newText",
-				weight: 7
-			}
-		];
-		return words;
-	}
+const Portfolio = (function () {
+  function makeWords() {
+    var words = [
+      {
+        text: "Object Oriented Programming",
+        weight: 12.3,
+      },
+      {
+        text: "css3",
+        weight: 8,
+      },
+      {
+        text: "javascript",
+        weight: 14,
+      },
+      {
+        text: "jquery",
+        weight: 3,
+      },
+      {
+        text: "programming",
+        weight: 7,
+      },
+      {
+        text: "python",
+        weight: 10,
+      },
+      {
+        text: "java",
+        weight: 9,
+      },
+      {
+        text: "Bootstrap",
+        weight: 15,
+      },
+      {
+        text: "PostgreSQL",
+        weight: 7,
+      },
+    ];
+    return words;
+  }
 
-	function makeWordCloud(words) {
-		$('.teaching-domains').jQCloud(words, {delay: 120});
-	}
+  function makeWordCloud(words) {
+    $(".teaching-domains").jQCloud(words, { delay: 120 });
+  }
 
-	function displayWordCloud() {
-		var count = 1;
-		$(window).on('scroll', function() {
-			var y_scroll_pos = window.pageYOffset;
-			var scroll_pos_test = 2700; // set to whatever you want it to be
-			var words = makeWords();
-			if (y_scroll_pos > scroll_pos_test && count <= 1) {
-				makeWordCloud(words);
-				count++;
-			}
-		});
-	}
+  function displayWordCloud() {
+    var count = 1;
+    $(window).on("scroll", function () {
+      var y_scroll_pos = window.pageYOffset;
+      var scroll_pos_test = 2700; // set to whatever you want it to be
+      var words = makeWords();
+      if (y_scroll_pos > scroll_pos_test && count <= 1) {
+        makeWordCloud(words);
+        count++;
+      }
+    });
+  }
 
-	function designForm() {
-		$("#my-modal form").addClass("my-form");
-	}
+  function designForm() {
+    $("#my-modal form").addClass("my-form");
+  }
 
-	function typeAnimation() {
-		Typed.new("#writing-text", {
-			strings: [
-				"am a Full-Stack Web Developer.", "love everything about code.", "also teach programming to people.", "solve problems."
-			],
-			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-			stringsElement: null,
-			// typing speed
-			typeSpeed: 1,
-			contentType: 'text',
-			callback: function() {
-				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
-			},
-			preStringTyped: function() {},
-			onStringTyped: function() {}
-		});
-	}
+  function typeAnimation() {
+    Typed.new("#writing-text", {
+      strings: [
+        "am a Full-Stack Web Developer.",
+        "love everything about code.",
+        "also teach programming to people.",
+        "solve problems.",
+      ],
+      // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+      stringsElement: null,
+      // typing speed
+      typeSpeed: 1,
+      contentType: "text",
+      callback: function () {
+        $("#writing-text").css({
+          color: "#fff",
+          "background-color": "#C8412B",
+        });
+      },
+      preStringTyped: function () {},
+      onStringTyped: function () {},
+    });
+  }
 
-	return {
-		displayWordCloud: displayWordCloud,
-		typeAnimation: typeAnimation
-	}
-
-}();
-
+  return {
+    displayWordCloud: displayWordCloud,
+    typeAnimation: typeAnimation,
+  };
+})();
 
 Portfolio.displayWordCloud();
 Portfolio.typeAnimation();
+document.addEventListener("DOMContentLoaded", function () {
+  let smoothScrollLinks = document.querySelectorAll(".smooth-scroll");
+  let headerHeight = document.querySelector("header").offsetHeight; // Assuming you have a <header> tag
+
+  smoothScrollLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      let target = document.querySelector(this.getAttribute("href"));
+
+      if (target) {
+        let topOffset = target.offsetTop - headerHeight; // Adjust the offset by the header height
+
+        window.scrollTo({
+          top: topOffset,
+          behavior: "smooth",
+        });
+      }
+    });
+  });
+});
+
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("scrolled", window.scrollY > 50);
+});
